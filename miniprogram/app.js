f4e0d8194c87685c4ef14a2dc7fc05d45d7b5da2
圣约翰大学 <94444674+xiangquhaibian1@users.noreply.github.com>
2024-07-15 08:48:25 +0800

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,7 +2,7 @@
  	onLaunch: function (options) {
 
  		if (!wx.cloud) {
- 			console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+ 			console.error('请使用 2.2.34 或以上的基础库以使用云能力')
  		} else {
  			wx.cloud.init({
  				env: 'card-8gdoyuexd3702a23',
